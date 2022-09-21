ee818b8f1cb74fddda1d97e6b03e16c7156ec3a0
Elmir Shamsutdinov <shamsut.elm@gmail.com>
2022-09-21 18:08:28 +0300

    added import from yaml

--- src/main/java/home/handlers/Tag.java
+++ src/main/java/home/handlers/Tag.java
@@ -16,4 +16,14 @@ public enum Tag {
     public String getTagName(){
         return tagName;
     }
+
+    public static Tag getTag(String tagName){
+        String tagFormatted = tagName.strip();
+        for (Tag tag : Tag.values()) {
+            if (tagFormatted.equals(tag.getTagName())) {
+                return tag;
+            }
+        }
+        return null;
+    }
 }
